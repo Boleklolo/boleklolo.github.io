d2eb6f719a9cb40d90b52d7c911ed36cfa6a5dc7
Boleklolo <mateuszmaz1986@protonmail.com>
2024-04-07 21:49:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -287,7 +287,6 @@ document.addEventListener("DOMContentLoaded", function(){
         var preloadS = document.getElementById("preloadoverlay");
         preloadB.style.display = "none";
         preloadS.style.display = "none";
-        setNight()
         setInterval(function() {
             var tip = document.getElementById("hideTip");
             tip.style.display = "none";
